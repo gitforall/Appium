c232b3c3a1578f6e5e758f67397d2af2bf18ad50
oohari <oohari@DESKTOP-RF5JEPV>
2016-12-17 21:55:24 +0530

    second commit

--- Appium/src/com/hari/appium/FaceBook.java
+++ Appium/src/com/hari/appium/FaceBook.java
@@ -3,17 +3,18 @@ package com.hari.appium;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-public class test {
+public class FaceBook {
 
 	
 	@Test
-	public void test1() throws MalformedURLException, InterruptedException{
+	public void test1() throws MalformedURLException{
 
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "Chrome");
@@ -24,6 +25,13 @@ public class test {
 		WebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 		driver.get("http://www.facebook.com");
 
+		driver.findElement(By.name("email")).clear();
+		driver.findElement(By.name("email")).sendKeys("hari.salamaddur@gmail.com");
+		
+		driver.findElement(By.name("pass")).clear();
+		driver.findElement(By.name("pass")).sendKeys("07G31a1250");
+		
+		driver.findElement(By.name("login")).click();
 		//driver.quit();
 	}
 	
